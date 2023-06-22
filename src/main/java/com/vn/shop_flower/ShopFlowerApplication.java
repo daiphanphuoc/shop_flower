587c4358ac99afc4e8ef97f2cf70adfe80698124
daiphanphuoc <daiphanphuoc@gmail.com>
2023-06-22 09:19:45 +0700

    start project

---
+++ src/main/java/com/vn/shop_flower/ShopFlowerApplication.java
@@ -0,0 +1,13 @@
+package com.vn.shop_flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFlowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFlowerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vn/shop_flower/ShopFlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.shop_flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFlowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
